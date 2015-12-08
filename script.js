45dee353573bab44be9e4cc401841eeea6f17f20
Sumant Sabada <ssabada@sumants-mbp.gateway.pace.com>
2015-12-08 16:58:20 -0600

    added button to turn images gray

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
   document.getElementById("playButton").addEventListener("click", function() {
         document.getElementById('playButton').style.display = 'none';
         document.getElementById('pauseButton').style.display = 'inline';
+		chrome.tabs.executeScript({code: "var x = document.getElementsByTagName('img'); for(var i = 0; i < x.length; i++) x[i].style.webkitFilter = 'grayscale(100%)';"});
     });
   document.getElementById("pauseButton").addEventListener("click", function() {
         document.getElementById('playButton').style.display = 'inline';
