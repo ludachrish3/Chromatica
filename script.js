c9f79aed9b9e0cb2e29370bfee6d618ed58ac693
Sumant Sabada <ssabada@sumants-mbp.gateway.pace.com>
2015-12-08 17:35:04 -0600

    removed dropdown

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
   document.getElementById("playButton").addEventListener("click", function() {
         document.getElementById('playButton').style.display = 'none';
         document.getElementById('pauseButton').style.display = 'inline';
+        document.getElementById('pauseDropdown').style.display = 'inline';
         var hue = document.getElementById('hue').value;
         var intensity = document.getElementById('intensity').value;	
         var command = "var x = document.getElementsByTagName('img'); for(var i = 0; i < x.length; i++) x[i].style.webkitFilter = 'hue-rotate("+hue+"deg) contrast("+intensity+"%)';"
@@ -10,6 +11,7 @@ document.addEventListener('DOMContentLoaded', function() {
   document.getElementById("pauseButton").addEventListener("click", function() {
         document.getElementById('playButton').style.display = 'inline';
         document.getElementById('pauseButton').style.display = 'none';
+        document.getElementById('pauseDropdown').style.display = 'none';
         chrome.tabs.executeScript({code: "var x = document.getElementsByTagName('img'); for(var i = 0; i < x.length; i++) x[i].style.webkitFilter = '';"});
     });
 });
