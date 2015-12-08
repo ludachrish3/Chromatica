06a0ded2b32893b8ebc5cc4e9a0340e13d7f8e87
Sumant Sabada <ssabada@sumants-mbp.gateway.pace.com>
2015-12-08 17:59:32 -0600

    sliders now change image while running

--- script.js
+++ script.js
@@ -1,17 +1,33 @@
 document.addEventListener('DOMContentLoaded', function() {
-  document.getElementById("playButton").addEventListener("click", function() {
-        document.getElementById('playButton').style.display = 'none';
-        document.getElementById('pauseButton').style.display = 'inline';
-        document.getElementById('pauseDropdown').style.display = 'inline';
-        var hue = document.getElementById('hue').value;
-        var intensity = document.getElementById('intensity').value;	
-        var command = "var x = document.getElementsByTagName('img'); for(var i = 0; i < x.length; i++) x[i].style.webkitFilter = 'hue-rotate("+hue+"deg) contrast("+intensity+"%)';"
+	document.getElementById("playButton").addEventListener("click", function() {
+		document.getElementById('playButton').style.display = 'none';
+		document.getElementById('pauseButton').style.display = 'inline';
+		document.getElementById('pauseDropdown').style.display = 'inline';
+		var hue = document.getElementById('hue').value;
+		var intensity = document.getElementById('intensity').value;	
+		var command = "var x = document.getElementsByTagName('img'); for(var i = 0; i < x.length; i++) x[i].style.webkitFilter = 'hue-rotate("+hue+"deg) contrast("+intensity+"%)';"
 		chrome.tabs.executeScript({code: command});
-    });
-  document.getElementById("pauseButton").addEventListener("click", function() {
-        document.getElementById('playButton').style.display = 'inline';
-        document.getElementById('pauseButton').style.display = 'none';
-        document.getElementById('pauseDropdown').style.display = 'none';
-        chrome.tabs.executeScript({code: "var x = document.getElementsByTagName('img'); for(var i = 0; i < x.length; i++) x[i].style.webkitFilter = '';"});
-    });
+	});
+	document.getElementById("pauseButton").addEventListener("click", function() {
+		document.getElementById('playButton').style.display = 'inline';
+		document.getElementById('pauseButton').style.display = 'none';
+		document.getElementById('pauseDropdown').style.display = 'none';
+		chrome.tabs.executeScript({code: "var x = document.getElementsByTagName('img'); for(var i = 0; i < x.length; i++) x[i].style.webkitFilter = '';"});
+	});
+	document.getElementById("hue").addEventListener("mouseup", function() {
+		if(document.getElementById('playButton').style.display =='none'){
+			var hue = document.getElementById('hue').value;
+			var intensity = document.getElementById('intensity').value;	
+			var command = "var x = document.getElementsByTagName('img'); for(var i = 0; i < x.length; i++) x[i].style.webkitFilter = 'hue-rotate("+hue+"deg) contrast("+intensity+"%)';"
+			chrome.tabs.executeScript({code: command});
+		}
+	});
+	document.getElementById("intensity").addEventListener("mouseup", function() {
+		if(document.getElementById('playButton').style.display =='none'){
+			var hue = document.getElementById('hue').value;
+			var intensity = document.getElementById('intensity').value;	
+			var command = "var x = document.getElementsByTagName('img'); for(var i = 0; i < x.length; i++) x[i].style.webkitFilter = 'hue-rotate("+hue+"deg) contrast("+intensity+"%)';"
+			chrome.tabs.executeScript({code: command});
+		}
+	});
 });
