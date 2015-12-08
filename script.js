7766c06f4905080abeeda5ca637aac1378bd0968
Sumant Sabada <ssabada@sumants-mbp.gateway.pace.com>
2015-12-08 17:08:51 -0600

    hue values change accordingn to scale

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ document.addEventListener('DOMContentLoaded', function() {
   document.getElementById("playButton").addEventListener("click", function() {
         document.getElementById('playButton').style.display = 'none';
         document.getElementById('pauseButton').style.display = 'inline';
-		chrome.tabs.executeScript({code: "var x = document.getElementsByTagName('img'); for(var i = 0; i < x.length; i++) x[i].style.webkitFilter = 'grayscale(100%)';"});
+        var hue = document.getElementById('hue').value;
+        var command = "var x = document.getElementsByTagName('img'); for(var i = 0; i < x.length; i++) x[i].style.webkitFilter = 'hue-rotate("+hue+"deg)';"
+		chrome.tabs.executeScript({code: command});
     });
   document.getElementById("pauseButton").addEventListener("click", function() {
         document.getElementById('playButton').style.display = 'inline';
